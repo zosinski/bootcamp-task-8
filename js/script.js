8e705fa873e401cee730038255910e56eafba688
Zbigniew Osiński <Zbigniew Osiski>
2016-09-22 23:03:35 +0200

    zadanie 8-5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = prompt('podaj zmienną a');
+var b = prompt('podaj zmienną b');
+
+var value = a * a + 2 * a * b - b * b;
+
+console.log('a^2 + 2ab - b^2 = ' + value);
+
+if(value > 0) {
+	console.log('wynik dodatni');
+}
+else if(value < 0) {
+	console.log('wynik ujemny');
+}
+else {
+	console.log('ni dodatni, ni ujemny');
+}
