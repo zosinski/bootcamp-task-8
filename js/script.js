91280a39d49bc8e7c74046e0256ddc5e7491bed9
Zbigniew Osiński <Zbigniew Osiski>
2016-09-22 22:05:47 +0200

    zadanie 8-4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+var a = prompt('Podaj długość podstawy trójkąta a=');
+var h = prompt('Podaj wysokość trójkąta h=')
+
+var triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a=' + a + ' i wysokości h=' + h + ' równa się: ' + triangleArea);
