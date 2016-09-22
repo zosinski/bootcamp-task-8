1b84d1adfc99cef26cf9f0d215d74444320d1685
Zbigniew Osiński <Zbigniew Osiski>
2016-09-22 23:50:05 +0200

    zadanie 8-6

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+do{
 
-var a = prompt('Podaj długość podstawy trójkąta a=');
-var h = prompt('Podaj wysokość trójkąta h=')
+	var a = prompt('Podaj długość podstawy trójkąta a=');
+	var h = prompt('Podaj wysokość trójkąta h=')
 
-var triangleArea = a * h / 2;
-console.log('Pole trójkąta o podstawie a=' + a + ' i wysokości h=' + h + ' równa się: ' + triangleArea);
+	var triangleArea = getTriangleArea(a, h);
+	if (triangleArea != 'nieprawidłowe dane'){
+		console.log('Pole trójkąta o podstawie a=' + a + ' i wysokości h=' + h + ' równa się: ' + triangleArea);
+		alert(triangleArea);
+	}
+	else {
+		console.log(triangleArea);
+		alert(triangleArea);
+	}
+
+} while (confirm('Jeszcze jedno obliczenie?'));
+
+function getTriangleArea(a, h){
+	if (a > 0 && h > 0){
+		return a * h / 2;
+	}
+	else {
+		return 'nieprawidłowe dane';
+	}
+}
