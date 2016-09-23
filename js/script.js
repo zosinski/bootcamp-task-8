bf60e24e93061b69abc00ae6465d7cd6a54199e4
Zbigniew Osiński <Zbigniew Osiski>
2016-09-23 18:45:36 +0200

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var names = femaleNames.concat(maleNames);
+console.log('połączona tablica: ' + names);
+
+do {
+	var newName = prompt('Podaj nowe imię');
+
+	if (names.indexOf(newName) == -1){
+		names.push(newName);
+		console.log('tablica z dodanym nowym imieniem: ' + names);
+		newName = '';
+	}
+	else {
+		alert('Imię już jest w tablicy');
+	}
+
+} while (newName !== '');
