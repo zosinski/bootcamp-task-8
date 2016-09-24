8d53a467a15a15972f93835140e8db0f184f515e
Zbigniew Osiński <Zbigniew Osiski>
2016-09-24 22:39:38 +0200

    dodana trzecia metoda z przypisywaniem wartości

--- js/script.js
+++ js/script.js
@@ -24,3 +24,19 @@ console.log('metoda B');
 value > 0 ? console.log('wynik dodatni') 
 : value < 0 ? console.log('wynik ujemny') 
 : console.log('ni dodatni, ni ujemny');
+
+/* -- method C with Conditional (ternary) Operator and assigning value -- */
+console.log('metoda C');
+
+var logMessage = value > 0 ? (
+	alert(value + ' - wynik dodatni'),
+	'wynik dodatni' // logMessage
+) : value < 0 ? (
+	alert(value + ' - wynik ujemny'),
+	'wynik ujemny' // logMessage
+) : (
+	alert(value + ' - wynik ni dodatni, ni ujemny'),
+	'ni dodatni, ni ujemny' // logMessage
+);
+
+console.log(logMessage);
