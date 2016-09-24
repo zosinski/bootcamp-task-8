38a2df5e4dd8b9838bb185edef5e379b5229c859
Zbigniew Osiński <Zbigniew Osiski>
2016-09-24 22:26:49 +0200

    dodany sposób ze skróconą wersją operatora if

--- js/script.js
+++ js/script.js
@@ -5,12 +5,22 @@ var value = a * a + 2 * a * b - b * b;
 
 console.log('a^2 + 2ab - b^2 = ' + value);
 
-if(value > 0) {
+/* -- method A -- */
+console.log('metoda A');
+
+if (value > 0) {
 	console.log('wynik dodatni');
 }
-else if(value < 0) {
+else if (value < 0) {
 	console.log('wynik ujemny');
 }
 else {
 	console.log('ni dodatni, ni ujemny');
 }
+
+/* -- method B with Conditional (ternary) Operator -- */
+console.log('metoda B');
+
+value > 0 ? console.log('wynik dodatni') 
+: value < 0 ? console.log('wynik ujemny') 
+: console.log('ni dodatni, ni ujemny');
